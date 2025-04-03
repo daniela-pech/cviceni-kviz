dc2332034b47931523b84860ae302255f74ca6c1
Daniela Pech <randakova.daniela@gmail.com>
2025-04-03 15:21:23 +0200

    vypracované cvičení - if, else, querySelector, classList.add

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const otazkaElement = document.querySelector('.question');
+const question =
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
+otazkaElement.textContent = question;
+const uzivatelOdpoved = prompt(question);
 
+const answerTextElement = document.querySelector('.answer__text');
+answerTextElement.textContent = uzivatelOdpoved;
+
+const answerElement = document.querySelector('.answer');
+
+if (uzivatelOdpoved === 'Bell') {
+  answerElement.classList.add('answer--correct');
+} else {
+  answerElement.classList.add('answer-wrong');
+}
